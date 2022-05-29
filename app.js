f144915fa9dd24148c9916b2b1e284e8e9d19b0f
trdxlucifer <singhdeepu566@gmail.com>
2022-05-29 22:04:56 +0530

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
+const msg = document.querySelector(".msg");
+const guess = document.querySelector("input");
+const btn = document.querySelector('.btn');
+let play = false;
+let newWords = "";
+let randWords = "";
 
+let Word = ["python", "c++", "javascript", "ruby", "hash", "typescript", "java", "android", "ios", "swift", "kubernet", "sql", "html", "css", "seaborn", "numpy", "pyscript", "reactjs", "vuejs", "angularjs", "nodejs", "expressjs", "nextjs", "flutter", "kottlen", "reactnative"];
+
+const createWords = () => {
+
+    let ranNum = Math.floor(Math.random() * Word.length);
+    let Newtemp = Word[ranNum];
+    // console.log(Newtemp.split(""))
+    return Newtemp;
+}
+
+
+const scrambleWords = (arr) => {
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        // console.log(temp);
+        let j = Math.floor(Math.random() * (i + 1));
+    
+
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+    return arr;
+}
+
+
+
+
+btn.addEventListener('click', function () {
+    if (!play) {
+        play = true;
+        btn.innerHTML = "Guess";
+        guess.classList.toggle('hidden');
+        newWords = createWords();
+        randWords = scrambleWords(newWords.split("")).join("");
+        //console.log(randWords);
+        msg.innerHTML = randWords;
+    
+
+    }
+
+})
 
 
 
